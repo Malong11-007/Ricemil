5cf14bee43b0768454ecf26d3d27fc1a65d3871a
Danyal Javed <daniyal.mir.dj@gmail.com>
2018-06-16 20:52:23 +0500

    db  changed

--- db.js
+++ db.js
@@ -3,7 +3,8 @@ var connection = mysql.createConnection({
   host: 'sql12.freemysqlhosting.net',
   user: 'sql12243197',
   password: '8YcqWUyKmf',
-  database : 'sql12243197'
+  database : 'sql12243197',
+  port : process.env.DB_PORT
 })
 
 
