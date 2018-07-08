13f52e22ff2bab496dcd5471a67b224a8c678e64
Danyal Javed <daniyal.mir.dj@gmail.com>
2018-07-08 20:09:12 +0500

    AJAX name validation

--- public/javascripts/register_val.js
+++ public/javascripts/register_val.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $("#username").keyup(function(){
     var name = $("#username").val();
 
-  $.post("http://localhost:5000/validate",{ username: name},function(response,status){
+  $.post("/validate",{ username: name},function(response,status){
       if(!response && status == "success"){
         $('#error_name').html('<div class="alert alert-danger"><span class="glyphicons glyphicons-warning-sign"></span>' + name + ' : USERNAME not available</div>');
       } else {
