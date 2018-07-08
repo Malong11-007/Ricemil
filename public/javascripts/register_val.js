43e05731c48846de38d6bc9248e6a40a55a11b0c
Danyal Javed <daniyal.mir.dj@gmail.com>
2018-07-08 20:12:50 +0500

    AJAX name validation

--- public/javascripts/register_val.js
+++ public/javascripts/register_val.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
   $("#username").keyup(function(){
     var name = $("#username").val();
 
-  $.post("/validate",{ username: name},function(response,status){
+  // $.post("http://localhost/validate",{ username: name},function(response,status){
+  $.post("https://ricemil.herokuapp.com/validate",{ username: name},function(response,status){
       if(!response && status == "success"){
         $('#error_name').html('<div class="alert alert-danger"><span class="glyphicons glyphicons-warning-sign"></span>' + name + ' : USERNAME not available</div>');
       } else {
